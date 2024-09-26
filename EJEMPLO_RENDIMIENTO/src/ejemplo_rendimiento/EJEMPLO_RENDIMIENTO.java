079978f49ee36f3b210cef1e26e99a9deee44151
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2024-09-26 14:17:05 -0600

    Add files via upload

---
+++ EJEMPLO_RENDIMIENTO/src/ejemplo_rendimiento/EJEMPLO_RENDIMIENTO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo_rendimiento;
+
+/**
+ * 23550382
+ * @author saul Bermudez Arzaga
+ * Estructura de datos
+ */
+public class EJEMPLO_RENDIMIENTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float[][] matriz = (
+                (1, 2, 3),
+                (2, 4, 7),
+        
+        
+        
+        
+        void traspuesta(float[][] d, int n){
+        int i, j;
+        for (i = n - 2; i>0; i--)
+        {
+            for (j = i + 1; j < n; j++)
+            {
+                float t;
+                t = d[j][i];
+                d[i][j] = d[j][i];
+                d[j][i] = t; 
+            }
+        }
+    }
+        
+    }
+    
+}
